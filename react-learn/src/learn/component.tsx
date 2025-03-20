9846a253545bbabe808eea1096fe02a8966217a1
NorthIgor-dev <77532282+NorthIgor-dev@users.noreply.github.com>
2025-03-20 11:12:12 +0300

    modified:   react-learn/src/learn/component.tsx

--- react-learn/src/learn/component.tsx
+++ react-learn/src/learn/component.tsx
@@ -1,12 +1,18 @@
-import { Component } from "react";
+import React from "react";
+
 
 /**
  * Component - это базовый класс для компонентов Реакт,
  * определенных как классы JavaScript. Компоненты классов по прежнему
  * поддерживаются Реакт, но их использование не рекомендуется в новом коде.
  */
-export class Greeting extends Component {
-    render() {
-        return <h1>Hello, {this.props.name}!</h1>;
-    }
-}
+interface GreetingProps {
+    name: string;
+};
+
+const Greeting: React.FC<GreetingProps> = ({ name }) => {
+    return <h1>Hello, {name}!</h1>;
+
+};
+
+export default Greeting;
